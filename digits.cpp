5c1ae0e8083a03ee7d457631372a072a8b5b12a2
sania sodhi <saniasodhi123@gmail.com>
2026-04-16 00:27:56 +0530

    Add files via upload

---
+++ digits.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the numbers:";
+    cin>>n;
+    int sum;
+    if(n%2==0){
+        cout<<"sum of all even no is:";
+        cout<<sum;
+        1
+    } else{
+        cout<<"not even numbers";
+    }
+}
